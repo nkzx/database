08d63220d157a6819ab0046b3e8525b2a7223133
Adrien Furnari <adrien@furnari.fr>
2024-05-02 10:32:38 +0200

    fix: removed overwrite of addFluentIndexes method

--- src/Bosnadev/Database/Schema/Blueprint.php
+++ src/Bosnadev/Database/Schema/Blueprint.php
@@ -26,36 +26,6 @@ class Blueprint extends \Illuminate\Database\Schema\Blueprint
     }
 
     /**
-     * Add the index commands fluently specified on columns.
-     *
-     * @return void
-     */
-    protected function addFluentIndexes()
-    {
-        foreach ($this->columns as $column) {
-            foreach (array('primary', 'unique', 'index', 'gin', 'gist') as $index) {
-                // If the index has been specified on the given column, but is simply
-                // equal to "true" (boolean), no name has been specified for this
-                // index, so we will simply call the index methods without one.
-                if ($column->$index === true) {
-                    $this->$index($column->name);
-
-                    continue 2;
-                }
-
-                // If the index has been specified on the column and it is something
-                // other than boolean true, we will assume a name was provided on
-                // the index specification, and pass in the name to the method.
-                elseif (isset($column->$index)) {
-                    $this->$index($column->name, $column->$index);
-
-                    continue 2;
-                }
-            }
-        }
-    }
-
-    /**
      * Specify an index for the table.
      *
      * @param  string|array  $columns
